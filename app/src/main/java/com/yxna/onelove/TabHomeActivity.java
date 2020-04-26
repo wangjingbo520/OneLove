241ac50629c257030c73ecf0db1208abef2ad05c
wangjingbo520 <390463560@qq.com>
2020-04-26 22:28:12 +0800

    init commit

---
+++ app/src/main/java/com/yxna/onelove/TabHomeActivity.java
@@ -0,0 +1,14 @@
+package com.yxna.onelove;
+
+import android.os.Bundle;
+
+import com.yxna.onelove.base.BaseActivity;
+
+public class TabHomeActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab_home);
+    }
+}
